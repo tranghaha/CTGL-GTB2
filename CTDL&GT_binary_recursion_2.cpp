177709bbf7bc824aff4f2e601f34cb1335ecfde4
tranghaha <125078331+tranghaha@users.noreply.github.com>
2023-04-06 10:34:53 +0700

    Update CTDL&GT_binary_recursion_2.cpp

--- CTDL&GT_binary_recursion_2.cpp
+++ CTDL&GT_binary_recursion_2.cpp
@@ -1,18 +1,16 @@
 #include <iostream>
 using namespace std;
  
-//hàm đệ quy tuyến tính
-int factorial(int n){
-  if(n == 0) return 1; // điểm dừng của hàm, nếu n == 0 thì kết thúc hàm và trả về 1
-  return n * factorial(n-1);
+int fib(int n){
+  if(n <= 2) return 1;
+  return fib(n - 1) + fib(n - 2);
 }
-//hàm main
+ 
 int main() {
-  int n;
-  cout<<"Nhập vào số giai thừa bạn muốn tính: ";
+  int kq,n;
+  cout<<"Nhập vị trí n muốn tìm trong dãy fibonacci: ";
   cin>>n;
-  int kq = factorial(n);//gọi hàm factorial() để tính giai thừa cho n và gán kết quả vào biến kq
-  cout<<"\nKết quả \n"<<n<<"! = "<<kq;
-   
-  cout<<"\n-----------------------------\n";
+  kq = fib(n);
+  cout<<"\nSố fibonacci ở vị trí "<<n<<" là: "<<kq;
+  cout<<"\n-----------------------------------\n";
 }
